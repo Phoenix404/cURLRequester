066ca1627d1d2c730a0085cd1e97a8e147941828
phoenix403 <manpreet2681995@gmail.com>
2017-09-29 18:48:51 +0200

    ddj

--- Src/Test.php
+++ Src/Test.php
@@ -29,10 +29,7 @@ $c 	= new cURLRequester("http://www.lina24.com/go_develope/", true);
 echo "\nCalling what are cookies\n";
 $data1 = $c->basicRequest("http://www.whatarecookies.com/cookietest.asp");
 
-echo "\nUseragent is : ".$c->getUserAgent()."\n";
 $c->setUserAgent();
-
-
 echo "\nUseragent is : ".$c->getUserAgent()."\n";
 
 //unset($c->result);
@@ -40,19 +37,15 @@ echo "\nUseragent is : ".$c->getUserAgent()."\n";
 
 $data1 = $c->basicRequest();
 echo "<textarea rows='10' cols='50'>".$data1."</textarea>";
-echo "\nServer is ";
-print_r($c->getServerType());
-echo "\nCookies are ";
-print_r($c->getCookies());
-echo "\nHttp Code: ";
-print_r($c->getHTTPCode());
-echo "\n";
-print_r($c->getHeaderStatus());
+echo "\nServer is ";print_r($c->getServerType());
+echo "\nCookies are ";print_r($c->getCookies());
+echo "\nHttp Code: ";print_r($c->getHTTPCode());
+echo "\n";print_r($c->getHeaderStatus());
 
 
 
 $c->init_cURL()->reset();
-echo "\nCalling Google\n";
+echo "\n<hr/>Calling Google\n";
 //$c->enableCache(true);
 
 $data2 = $c
@@ -61,10 +54,10 @@ $data2 = $c
         ->followLocation(true)
         ->basicRequest("https://www.google.it/?gfe_rd=cr&dcr=0&ei=SEfFWcTsKrLBXoHSnagK&gws_rd=ssl");
 echo "<textarea rows='10' cols='50'>".$data2."</textarea>";
-echo "\nServer is ";
-print_r($c->getServerType());
-echo "\nCookies are ";
-print_r($c->getCookies());
+echo "\nServer is ";print_r($c->getServerType());
+echo "\nCookies are ";print_r($c->getCookies());
+echo "\nHttp Code: ";print_r($c->getHTTPCode());
+echo "\n";print_r($c->getHeaderStatus());
 
 //echo $c->getResult();
 
